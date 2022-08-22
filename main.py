4d1f4f6b2645d7cd74925e5d4cde7d6e906353af
1110236 <915840469@qq.com>
2022-08-22 17:05:04 +0800

    Self tested with 6 conditions. See https://docs.google.com/document/d/1vSX8RkQCwSj7wS5ZLbNv7_VMcx1qsxHJ22KyVKRuFp0/edit# for details.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.STAND + '-' + Config.NOREAD)
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.WALK_STAIRPATH + '-' + Config.NOREAD)
     threadPupilDataReader.start()
 
