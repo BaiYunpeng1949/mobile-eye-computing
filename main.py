b2c9a254b41a76b3ee40d7f3f910715051bc2d4a
1110236 <915840469@qq.com>
2022-08-23 16:40:00 +0800

    Finished the self test on StairPath scenarios, and added dynamical sampling rate function.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.WALK_STAIRPATH + '-' + Config.NOREAD)
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.WALK_SIMPATH + '-' + Config.HARD)
     threadPupilDataReader.start()
 
