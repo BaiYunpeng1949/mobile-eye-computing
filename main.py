cb1ef3bf40cffb56e9561c2a9ea12d275c984352
1110236 <915840469@qq.com>
2022-08-24 18:55:59 +0800

    Quick tests with Lin, Ben, and Zhou Chen.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.WALK_SIMPATH + '-' + Config.HARD)
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.WALK_STAIRPATH + '-' + Config.HARD)
     threadPupilDataReader.start()
 
