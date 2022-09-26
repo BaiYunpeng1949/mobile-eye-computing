0c0994ebe2de735468dfbf51518c1e401a7264d4
1110236 <915840469@qq.com>
2022-09-26 15:01:25 +0800

    Tested with Runze

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.LOWLUX + '-' + Config.NOBACK)
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.HIGHLUX + '-' + Config.ONEBACK)
     threadPupilDataReader.start()
 
