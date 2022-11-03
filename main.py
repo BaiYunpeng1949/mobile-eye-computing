a4556b98754019327d6eee8001c11d562e6a2787
1110236 <915840469@qq.com>
2022-11-03 11:57:49 +0800

    Added the name into the data collection part.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.HIGHLUX + '-' + Config.THREEBACK)
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-ParticipantName-' + Config.HIGHLUX + '-' + Config.THREEBACK)
     threadPupilDataReader.start()
 
