49a27e74902e58f6cad4a2453cb0c9329e9ae65e
1110236 <915840469@qq.com>
2022-11-29 14:43:42 +0800

    Tried with the new data from Jen. Fixed some minor problems of indexing in the de-blinking function.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.LOWLUX + '-' + Config.ONEBACK + '-P09')
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.LOWLUX + '-' + Config.ONEBACK + '-09')
     threadPupilDataReader.start()
 
