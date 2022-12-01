739f740e48282109bdb37fee056a24e5fc40ab10
1110236 <915840469@qq.com>
2022-12-01 13:58:01 +0800

    Tested with myself and Junjie by Jennifer.

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import Config
 import ReadRawPupilData
 
 if __name__ == '__main__':
-    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.LOWLUX + '-' + Config.ONEBACK + '-09')
+    threadPupilDataReader = ReadRawPupilData.DataReadThread(setting='-' + Config.HIGHLUX + '-' + Config.THREEBACK + '-11')
     threadPupilDataReader.start()
 
